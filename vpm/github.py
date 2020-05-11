7f3b1205c2eb1811a07e51581e7d05bd2223b7f7
LudwigCRON <ludwig.cron@gmail.com>
2020-05-11 10:59:39 +0200

    remove token from code database: - token revoked - generate a new token - place token in env variables

--- vpm/github.py
+++ vpm/github.py
@@ -10,19 +10,16 @@ from urllib.request import urlopen, Request
 # 60 reqs/h if unauth and 5000 reqs/h if basic
 # For search api respectivly 10 and 30 reqs/min
 
-# valid user-agent is mandatory
-USERAGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36 Edg/81.0.416.68"
-
 # user/repo or user/repo as username:password
 BASEURL = "https://api.github.com/repos/%s/git/trees/master?recursive=1"
 
 # basic auth is deprecated and will be removed
-VPM_TOKEN = "b13284edd3cb7a32ff5b8d167ff7e02e5ef62c3d"
+VPM_GITHUB_TOKEN = os.getenv("VPM_GITHUB_TOKEN")
 
 
 def github_request(url):
     req = Request(url)
-    req.add_header("Authorization", "token %s" % VPM_TOKEN)
+    req.add_header("Authorization", "token %s" % VPM_GITHUB_TOKEN)
     res = urlopen(req)
     return json.load(res)
 
